34790b05ddc6085491d2db28273f85ed277e40d4
AndresG2412 <cgaviria930@gmail.com>
2026-04-30 11:18:15 -0500

    new: importaciones

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Hero from "./components/Hero";
 import Carrousel from "./components/Carrousel";
 import Categorias from "./components/Categorias";
 import Performance from "./components/Performance";
+import Location from "./components/Location";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <Carrousel />
       <Categorias />
       <Performance />
+      <Location />
+      <Footer />
     </main>
   );
 }
